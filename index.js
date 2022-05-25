539e9b64112f0656935bf409d7f175c059a60895
reyys <reyyy0207@gmail.com>
2022-05-25 19:43:06 +0700

    Big update

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+import { toJavanese } from "/node_modules/carakanjs/dist/esm/index.js";
+
 const textJawa = document.querySelector('.text-jawa');
 const inputJawa = document.querySelector('.jawa')
+const copyBtn = document.querySelector('.copy')
+const copiedText = document.querySelector('.copied')
 
-textJawa.innerHTML = `<aksara-jawa latin="sugeng rawuh" /> `
+textJawa.innerHTML = toJavanese('sugeng rawuh')
 
 inputJawa.addEventListener('input', (e) => {
     let x = e.target.value
-    textJawa.innerHTML = `<aksara-jawa latin="${x}" />`
+    textJawa.innerHTML = toJavanese(x)
 })
 
-console.log(textJawa)
+copyBtn.addEventListener('click', () => {
+    textJawa.select();
+    document.execCommand("copy")
+    copiedText.classList.add('active')
+    setTimeout(() => {
+        copiedText.classList.remove('active')
+    }, 500)
+})
